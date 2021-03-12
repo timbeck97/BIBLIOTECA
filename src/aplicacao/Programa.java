9fa51d6db2a663d4d3821eea2975bc8f4dd76501
Tim Beck <timbeck1997@hotmail.com>
2021-03-11 21:06:24 -0300

    Teste de classe programa e classe tela para imprimir acervo

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -7,8 +7,8 @@ public class Programa {
 	public static void main(String[] args) {
 		
 		Biblioteca b=new Biblioteca();
-		
-		
+		b.adicionaLivro("Livro teste");
+		Tela.imprimeAcervo(b.getAcervo());
 		
 
 	}
